1ef898732238ec1c8b7482e0b177cbb7f27ec55e
via-photo <admin@viaphoto.ru>
2025-06-19 11:06:51 +0300

    Add files via upload

--- api_server.py
+++ api_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
+from fastapi import FastAPI, HTTPException, Depends, Header, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional, Dict, Any
